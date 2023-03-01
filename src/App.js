cf5256240b9786538d40ca00b167f81ca4e898db
BogdanK2 <116885836+BogdanKwasnicki@users.noreply.github.com>
2023-03-01 23:56:19 +0000

    Added a nav bar to home page
    Worked in the "NavHeader" function, that adds a nav bar to home page, which includes the elanco logo, page title, and the menu buttons for navigating between the pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+function NavHeader(){
+  return(
+    <>
+      <nav>
+        <div class="nav_logo_container">
+          <img class="nav_logo" src="/Images/App/TransparentElancoLogo.png"></img>
+          <a href="https://www.elanco.com/en-us" class="nav_logo_Button"></a>
+          <div class="nav_logo_DarkBlueCircle"></div>
+        </div>
+        <div class="nav_title_container">
+          <img class="page_icon" src="/Images/App/farmlogo2.png"></img>
+          <h1 class="page_title">FARM DASHBAORD</h1>
+        </div>
+        <div class="nav_links">
+          <a href="">Home</a>
+          <a href="">Land plots</a>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 function App() {
-  return null;
+  return(
+    <>
+      <NavHeader></NavHeader>
+    </>
+  );
 }
 
 export default App;
