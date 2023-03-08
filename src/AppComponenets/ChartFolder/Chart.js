1bb7f38acfad4e7df751ec13eb47728e893ea436
Samuel12372 <samleoknowles@outlook.com>
2023-03-08 11:55:13 +0000

    working on chart js

--- src/AppComponenets/ChartFolder/Chart.js
+++ src/AppComponenets/ChartFolder/Chart.js
@@ -1,13 +1,37 @@
 import './Chart.css';
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
-function Chart(){
-  return(
-    <>
-      <div class="Chart_box fill_in_box">
-        <a>Sam and music joe WORKSPACE</a>
-      </div>
-    </>
-  );
+function ChartDisplay(){
+  return(null)
+   
 }
 
-export default Chart;
+//export default ChartDisplay;
+
+       
+const BarChart = () => {
+  const labels = ["January", "February", "March", "April", "May", "June","July","August","September","October","November","December"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        
+        label: "Temperature",
+        backgroundColor: 'rgba(54, 162, 235, 1)',
+        borderColor: "rgb(255, 99, 235)",
+        data: [5, 10, 5, 2, 20, 30, 45, 40, 35, 20, 15, 25],
+        borderWidth: 1
+
+      },
+    ],
+  };
+  return (
+    <div className = "barChart">
+      <Bar data={data} />
+    </div>
+  );
+};
+
+  
+export default BarChart;
