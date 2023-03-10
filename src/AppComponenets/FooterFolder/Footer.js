3b691413dd9e5c8eb136d669ef4fe287d06e2754
C2017141 <C2017141@hallam.shu.ac.uk>
2023-03-10 18:12:53 +0000

    completed creating footer

--- src/AppComponenets/FooterFolder/Footer.js
+++ src/AppComponenets/FooterFolder/Footer.js
@@ -1,10 +1,21 @@
 import './Footer.css';
 
+var script = document.createElement('script');
+script.src = 'https://kit.fontawesome.com/1d482a1a8c.js';
+script.setAttribute('crossorigin', 'anonymous');
+document.head.appendChild(script);
+
+
 function Footer(){
   return(
     <>
       <footer>
-        <a>FREE WORKSPACE FOR FOOTER</a>
+        <div class = "mediaLinks">
+          <h1> Connect with Elanco : </h1>
+          <a href='https://www.instagram.com/weareelanco/' target="_Blank"><i class="fa-brands fa-instagram"></i></a> 
+        <a href='https://twitter.com/Elanco/' target="_Blank" ><i class="fa-brands fa-twitter"></i></a> 
+        <a href='https://www.facebook.com/elancoanimalhealth/' target="_Blank" ><i class="fa-brands fa-facebook"></i></a>
+        </div>
       </footer>
     </>
   );
