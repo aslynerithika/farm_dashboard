326349633c21d34eb82974e71d211b8def204664
aslynerithika <c2012616@hallam.shu.ac.uk>
2023-03-13 22:56:04 +0000

    imagecarousel

--- src/AppComponenets/LandPlotSelectFolder/LandPlotSelect.js
+++ src/AppComponenets/LandPlotSelectFolder/LandPlotSelect.js
@@ -1,12 +1,98 @@
 import './LandPlotSelect.css';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+import slide_image_1 from "./assets/P1.jpg";
+import slide_image_2 from "./assets/P2.jpg";
+import slide_image_3 from "./assets/P3.jpg";
+import slide_image_4 from "./assets/P4.jpg";
+import slide_image_5 from "./assets/P5.jpg";
+import slide_image_6 from "./assets/P6.jpg";
+import slide_image_7 from "./assets/P7.jpg";
+import slide_image_8 from "./assets/P8.jpg";
+import slide_image_9 from "./assets/P9.jpg";
+import slide_image_10 from "./assets/P10.jpg";
+
 
 function LandPlotSelect(){
   return(
-    <>
       <div class="LandPlotSelect_box fill_in_box">
-        <a>Aslyne WORKSPACE</a>
+        <div className="container">
+        <h1 className="heading">Land Plots</h1>
+        <Swiper
+          effect={ 'coverflow' }
+          grabCursor={ true }
+          centeredSlides={ true }
+          loop={ true }
+          slidesPerView={ 'auto' }
+          coverflowEffect={
+            {
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 2.5,
+            } }
+            pagination={{ el: '.swiper-pagination', clickable: true }}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+              clickable: true,
+            }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className='swiper_container'
+        >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_8} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_9} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_10} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+          <ion-icon name="arrow-back"></ion-icon>
+
+          </div>
+          <div className="swiper-button-next slider-arrow">
+
+            <ion-icon name="arrow-forward"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+        </Swiper>
+     </div>
       </div>
-    </>
   );
 }
 
