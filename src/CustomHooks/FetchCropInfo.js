43e1c506f03c149b6b822a8eafddd4b67f13bf98
C2017141 <C2017141@hallam.shu.ac.uk>
2023-03-25 16:46:14 +0000

    moving new crops to  new branch

--- src/CustomHooks/FetchCropInfo.js
+++ src/CustomHooks/FetchCropInfo.js
@@ -1,49 +1,171 @@
 const cropList = {
-    "Wheat" : {
-      name  : "Wheat",
-      desc  : "Wheat is a common grain that provides carbohydrates, fiber, vitamins, and minerals Eating wheat-based products can help maintain healthy blood sugar levels and reduce the risk of chronic diseases",
-      Yield : "£450",
-      Cost  : "£100",
-      GrowTime : "45 Days",
-      image : "../Images/Cropimages/Wheat.jpeg"
+  "Wheat" : {
+    name  : "Wheat",
+    desc  : "Wheat is a common grain that provides carbohydrates, fiber, vitamins, and minerals Eating wheat-based products can help maintain healthy blood sugar levels and reduce the risk of chronic diseases",
+    Yield : "£450",
+    Cost  : "£100",
+    GrowTime : "45 Days",
+    image : "../Images/Cropimages/Wheat.jpeg"
+
+  },
+  "Rice"  : {
+    name  : "Rice",
+    desc  : "Rice is a staple food for more than half of the world's population, particularly in Asia. It is a cereal grain that grows in paddies, and is commonly eaten boiled or steamed as a side dish or base for many dishes",
+    Yield : "£250",
+    Cost  : "£50",
+    GrowTime : "30 Days",
+    image : "../Images/Cropimages/Rice.jpeg"
+
+  },
+  "Corn"  : {
+    name  : "Corn",
+    desc  : "Corn, also known as maize, is a cereal grain that is a member of the grass family, Poaceae. It is one of the most widely cultivated crops in the world, and it is used for a variety of purposes such as food, animal feed, and fuel",
+    Yield : "£400",
+    Cost  : "£75",
+    GrowTime : "30 Days",
+    image : "../Images/Cropimages/Corn.jpeg"
+
+  },
+  "Barley" : {
+    name  : "Barley",
+    desc  : "Barley grains are small and oval-shaped, with a hard outer layer called the husk, and a soft starchy interior, Barley is a good source of dietary fiber and vitamins and it has been linked to various health benefits, such as reducing the risk of heart disease and improving digestion.",
+    Yield : "£500",
+    Cost  : "£150",
+    GrowTime : "60 Days",
+    image : "../Images/Cropimages/Barley.jpeg"
+       
+  },
+  "Oats" : {
+    name  : "Oats",
+    desc  : "Oats are a type of cereal grain that is commonly used as a food source for both humans and livestock. They are a rich source of nutrients, including fiber, protein, vitamins, and minerals, making them a popular choice for people looking to improve their overall health.",
+    Yield : "£300",
+    Cost  : "£80",
+    GrowTime : "30 Days",
+    image : "../Images/Cropimages/Oats.jpeg"
+          
+
+  },
+
+  "Straw" : {
+    name  : "Straw",
+    desc  : "A straw crop refers to any plant that is specifically grown to produce straw, which is the dried stalks and stems of certain cereal crops like wheat, barley, and rice. The straw is often used as a raw material for various purposes, such as animal feed and bedding.",
+    Yield : "£250",
+    Cost  : "£50",
+    GrowTime : "28 Days",
+    image : "../Images/Cropimages/straw.jpeg"
+          
+
+  },
+
+  "Sugar Beet" : {
+    name  : "Sugar Beet",
+    desc  : "Sugar beet is a plant that is primarily grown for its sugar-rich root. The root is harvested and processed to extract the sugar, which is then used for various purposes such as food production, sweeteners, and biofuels.",
+    Yield : "£280",
+    Cost  : "£70",
+    GrowTime : "35 Days",
+    image : "../Images/Cropimages/Sugar-Beet .jpeg"
+          
+
+  },
+
+  "Oilseed" : {
+    name  : "Oilseed",
+    desc  : "Oilseeds are plants that produce seeds with a high oil content, which can be extracted and used for a variety of purposes. Common oilseeds include soybeans, canola, sunflower, rapeseed, and cottonseed. These seeds are rich in healthy fats, vitamins, and minerals, and are a valuable source of protein for both humans and animals.",
+    Yield : "£320",
+    Cost  : "£85",
+    GrowTime : "25 Days",
+    image : "../Images/Cropimages/Oilseed.jpeg"
+          
+
+  },
+
+  "Peas" : {
+    name  : "Peas",
+    desc  : "Peas are a type of legume that are widely consumed and cultivated around the world. They are an excellent source of dietary fiber, protein, vitamins. Peas can be eaten fresh, frozen, or dried, and are used in a variety of dishes, including soups, stews, salads, and curries.",
+    Yield : "£370",
+    Cost  : "£110",
+    GrowTime : "35 Days",
+    image : "../Images/Cropimages/peas.jpeg"
+          
+
+  },
+
+  "Field Beans" : {
+    name  : "Field Beans",
+    desc  : "Field beans are a type of legume that are grown as a crop in many parts of the world. They are high in protein, fiber, and are used as a food source for both humans and animals. Field beans are also commonly used as a cover crop, as they can help improve soil health and reduce erosion.",
+    Yield : "£445",
+    Cost  : "£100",
+    GrowTime : "40 Days",
+    image : "../Images/Cropimages/Field-beans.jpeg"
+          
+
+  },
+
+
+  "Vegetables" : {
+    name  : "Vegetables",
+    desc  : "Vegetables are a diverse group of plants that are cultivated for their edible parts, such as leaves, stems, roots, or bulbs. They are a rich source of vitamins, minerals, and fiber, and are an important part of a healthy diet. Vegetables come in a wide range of colors, flavors, and textures, and can be eaten raw or cooked in a variety of ways. ",
+    Yield : "£350",
+    Cost  : "£75",
+    GrowTime : "35 Days",
+    image : "../Images/Cropimages/vegetables.jpeg"
+          
+
+  },
+
+  "Fruits" : {
+    name  : "Fruits",
+    desc  : "Fruits are the sweet and fleshy reproductive structures of flowering plants. They are packed with vitamins, minerals, fiber, and antioxidants, and are an essential part of a healthy diet. Fruits come in a wide variety of shapes, sizes, and flavors, and can be eaten fresh, dried, or cooked in various recipes. ",
+    Yield : "£300",
+    Cost  : "£60",
+    GrowTime : "55 Days",
+    image : "../Images/Cropimages/Fruits.jpeg"
+          
+
+  },
+
+
+  "Potatoes" : {
+    name  : "Potatoes",
+    desc  : "Potatoes are a starchy root vegetable that are widely consumed and cultivated around the world. They are a rich source of carbohydrates, fiber, and essential nutrients such as vitamin C, potassium, and iron. Potatoes are a versatile food that can be prepared in many different ways, including boiling, baking, frying, and mashing ",
+    Yield : "£360",
+    Cost  : "£80",
+    GrowTime : "42 Days",
+    image : "../Images/Cropimages/Potato.jpeg"
+          
+
+  },
+
+
+
+  "Rye" : {
+    name  : "Rye",
+    desc  : "Rye is a cereal grain that is closely related to wheat and barley. It is a hardy crop that can be grown in colder climates and poor soils, making it an important crop in many regions around the world. Rye is a good source of fiber, protein, and essential nutrients such as iron and zinc. ",
+    Yield : "£265",
+    Cost  : "£65",
+    GrowTime : "28 Days",
+    image : "../Images/Cropimages/Rye.webp"
+          
+
+  },
+
+
+
+  "Millet" : {
+    name  : "Millet",
+    desc  : "Millet is a small, gluten-free cereal grain that is widely grown and consumed in many parts of the world. It is a good source of protein, fiber, and essential nutrients such as magnesium and phosphorus. Millet is a versatile ingredient that can be used in a variety of dishes, including porridge, bread, and pilaf ",
+    Yield : "£370",
+    Cost  : "£85",
+    GrowTime : "35Days",
+    image : "../Images/Cropimages/Millet.jpeg"
+          
+
+  },
+
+
   
-    },
-    "Rice"  : {
-      name  : "Rice",
-      desc  : "Rice is a staple food for more than half of the world's population, particularly in Asia. It is a cereal grain that grows in paddies, and is commonly eaten boiled or steamed as a side dish or base for many dishes",
-      Yield : "£250",
-      Cost  : "£50",
-      GrowTime : "30 Days",
-      image : "../Images/Cropimages/Rice.jpeg"
-  
-    },
-    "Corn"  : {
-      name  : "Corn",
-      desc  : "Corn, also known as maize, is a cereal grain that is a member of the grass family, Poaceae. It is one of the most widely cultivated crops in the world, and it is used for a variety of purposes such as food, animal feed, and fuel",
-      Yield : "£400",
-      Cost  : "£75",
-      GrowTime : "30 Days",
-      image : "../Images/Cropimages/Corn.jpeg"
-  
-    },
-    "Barley" : {
-      name  : "Barley",
-      desc  : "Barley grains are small and oval-shaped, with a hard outer layer called the husk, and a soft starchy interior, Barley is a good source of dietary fiber and vitamins and it has been linked to various health benefits, such as reducing the risk of heart disease and improving digestion.",
-      Yield : "£500",
-      Cost  : "£150",
-      GrowTime : "60 Days",
-      image : "../Images/Cropimages/Barley.jpeg"
-         
-    },
-    "Oats" : {
-      name  : "Oats",
-      desc  : "Oats are a type of cereal grain that is commonly used as a food source for both humans and livestock. They are a rich source of nutrients, including fiber, protein, vitamins, and minerals, making them a popular choice for people looking to improve their overall health.",
-      Yield : "£300",
-      Cost  : "£80",
-      GrowTime : "30 Days",
-      image : "../Images/Cropimages/Oats.jpeg"
-            
- 
-    }
+
+
 }
-    export default cropList;
+  export default cropList;
+
