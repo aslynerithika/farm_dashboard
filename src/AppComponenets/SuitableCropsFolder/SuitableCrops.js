713ba1a774171b3aeb31b4cccf6154a2eaf79996
BennettJack <97344648+BennettJack@users.noreply.github.com>
2023-03-30 17:08:07 +0100

    Images, growth time, title added
    Images, growth time, title added to suitable crops

--- src/AppComponenets/SuitableCropsFolder/SuitableCrops.js
+++ src/AppComponenets/SuitableCropsFolder/SuitableCrops.js
@@ -1,6 +1,7 @@
 import './SuitableCrops.css';
 import React, { useEffect, useState } from 'react';
 import {Container} from "@mui/material";
+import Tooltip from '@mui/material/Tooltip';
 
 
 let lowerPHLimit;
@@ -54,34 +55,34 @@ const oats = Crop('Oats','6.2','6.6','15','25','25',
     '75','10','40','80','300','30','1','Oats.jpeg')
 
 const straw = Crop('Straw','5.8','6.3','18','24','35',
-    '64','20','60','50','250','28','1','#')
+    '64','20','60','50','250','28','1','Straw.jpeg')
 
 const sugarBeat = Crop('Sugar Beat','4.7','7.2','15','22','30',
-    '50','25','55','70','280','35','1','#')
+    '50','25','55','70','280','35','1','Sugar-Beet .jpeg')
 
 const oilseed = Crop('Oil Seed','5.0','7.8','20','26','28',
-    '68','30','100','85','320','25','1','#')
+    '68','30','100','85','320','25','1','Oilseed.jpeg')
 
 const peas = Crop('Peas','3.6','5.0','15','20','15',
-    '60','25','60','110','370','35','1','#')
+    '60','25','60','110','370','35','1','Peas.jpeg')
 
 const fieldBeans = Crop('Field Beans','6.7','9.7','20','32','15',
-    '80','15','80','100','445','40','1','#')
+    '80','15','80','100','445','40','1','Field-beans.jpeg')
 
 const vegetables = Crop('Vegetables','3','7.5','18','25','20',
-    '55','35','65','75','350','35','1','#')
+    '55','35','65','75','350','35','1','Vegetables.jpeg')
 
 const fruits = Crop('Fruits','4.8','7.8','15','30','30',
-    '70','20','65','60','300','55','1','#')
+    '70','20','65','60','300','55','1','Fruits.jpeg')
 
 const potatoes = Crop('Potatoes','3.6','6.6','9','29','28',
-    '68','20','80','80','360','42','1','#')
+    '68','20','80','80','360','42','1','Potato.jpeg')
 
 const rye = Crop('Rye','2.8','4.2','10','27','18',
-    '48','15','55','65','265','28','1','#')
+    '48','15','55','65','265','28','1','Rye.webp')
 
 const millet = Crop('Millet','2.3','7.3','13','28','45',
-    '85','35','95','85','370','35','1','#')
+    '85','35','95','85','370','35','1','Millet.jpeg')
 
 const cropArray= [wheat, rice, corn, barley, oats, straw, sugarBeat, oilseed, peas, fieldBeans, vegetables, fruits, potatoes, rye, millet];
 let bestCropArray = cropArray;
@@ -160,38 +161,41 @@ function GetOpacity(key){
 
     test = 1 - ((key*100)/25)/50;
     console.log("opacity test" + test);
-    if(test > .2) {
+    if(test > .4) {
         return {opacity: test};
     }
     else{
-        return{opacity: .2}
+        return{opacity: .4}
     }
 }
-
-function CreateCropInfoBox(crop, key){
+const background = .4
+const CreateCropInfoBox = (crop, key) =>{
     const [isShown, showInformation] = useState(false);
     return(
         <>
+            
+            <div className ="cropInfoBox" style={GetOpacity(key)} id={'test' + key}>
+                <div className={"column cropRank"} >
+                    <h3 className = {'cropRankValue' + key}> {key + 1}{GetRankExtension(key)} </h3>
 
-            <div className ="cropInfoBox" style={GetOpacity(key)} >
-                <div className="column cropRank">
-                    <h3 className = {'cropRankValue' + key}>{key + 1}{GetRankExtension(key)} </h3>
-                    <p><img src={"/Images/App/information.png"}
-                            onMouseEnter={() => showInformation(true)}
-                            onMouseLeave={() => showInformation(false)}
-                    ></img>   £{crop.cropYield}</p>
+                    <p><Tooltip className="tooltip" title="Maintainance cost" arrow>
+                        <li> <img src="/Images/App/information.png"></img>{crop.GrowTime}</li>
+                    </Tooltip>  £{crop.cropYield}</p>
                 </div>
                 <div className="column cropImage">
                     <img src={"/Images/Cropimages/"+crop.imageURL} width="100%" height="100%" alt={crop.name}></img>
                 </div>
                 <div className="column cropInfo">
-                    <p className="cropInfo">
+                    
                         <h3>{crop.name}</h3>
                         <p>This is the description of the crops. The crop description will be shown here.</p>
                         <p className = "cropIdealConditions">PH: {crop.lowerPHLimit} - {crop.upperPHLimit} | Humidity: {crop.lowerHumidityLimit}%
                             - {crop.upperHumidityLimit}% | {crop.lowerTempLimit}°C - {crop.upperTempLimit}°C</p>
-                    </p>
-
+                </div>
+                <div className="column cropGrowthTime">
+                    <img className={"imgTimer"} src={"/Images/App/timer-icon.png"} height={"50px"} width={"50px"}></img>
+                    <p>Growth time: </p>
+                    <p>{crop.growthTime} days</p>
                 </div>
 
 
@@ -232,7 +236,9 @@ function SuitableCrops(){
 
     return(
         <>
+            
             <div class="suitable_crops_box fill_in_box">
+                <h1>Most Suitable Crops</h1>
                 <a></a>
                 {bestCropArray.map((crop, key) =>(CreateCropInfoBox(crop, key)))}
 
